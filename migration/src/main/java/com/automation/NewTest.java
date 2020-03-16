51e1027ddfc13a74c706146c84030e0344faacee
luisemora9 <42556679+luisemora9@users.noreply.github.com>
2020-03-16 02:37:04 -0700

    code

---
+++ migration/src/main/java/com/automation/NewTest.java
@@ -0,0 +1,39 @@
+package com.automation;
+
+import org.testng.annotations.Test;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+
+public class NewTest {
+	
+	private WebDriver driver;
+	
+	  @BeforeClass
+	  public void beforeClass() {
+		  driver = new ChromeDriver();
+	  }
+	  
+	  @AfterClass
+	  public void afterClass() {
+		  driver.quit(); //testeee
+	  }
+	  
+  @Test
+  public void f() {
+      driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+      driver.get("http://www.google.com");
+
+      String search_text = "Google Search";
+      WebElement search_button = driver.findElement(By.name("btnK"));
+
+  }
+}
